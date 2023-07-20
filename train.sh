85762fc80e016966a4470accffa37a22fc6370cc
Gavin Sun <gavinsun0921@foxmail.com>
2023-07-20 14:51:35 +0800

    sync for gitignore

--- train.sh
+++ train.sh
@@ -4,11 +4,12 @@
 #--label_dir $input_path/labels \
 #--image_dir $input_path/imgs \
 #--label_nc 29 --no_instance --use_vae --batchSize 4
-input_path="../dataset/Jittor/train_resized"
-CUDA_VISIBLE_DEVICES=0 python train.py \
+input_path="/home/gavin/Documents/Dataset/Jittor2023/train_resized"
+CUDA_VISIBLE_DEVICES=1 python train.py \
       --name basic \
       --dataset_mode custom \
       --label_dir $input_path/labels \
       --image_dir $input_path/imgs \
       --label_nc 29  --no_instance  --batchSize 4 \
-      --continue_train  --which_epoch latest   --lr 0.0001
+      --lr 0.0001 \
+      --continue_train --which_epoch latest
